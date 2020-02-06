cb508e59564359f136b0a91ca363b5662bca6a5d
Your Name <you@example.com>
2020-02-05 21:41:05 -0300

    .

--- Resources/views/loader/aside/report.blade.php
+++ Resources/views/loader/aside/report.blade.php
@@ -7,6 +7,7 @@
     <div class="list-group-item list-group-item-accent-secondary bg-light text-center font-weight-bold text-muted text-uppercase small">Items</div>
     <a href="{{ route('reports.items') }}" class="list-group-item list-group-item-accent-dark list-group-item-divider list-group-item-action">Simples</a>
     <a href="{{ route('reports.items.full') }}" class="list-group-item list-group-item-accent-dark list-group-item-divider list-group-item-action">Detalhado</a>
+    @loader(['loader_path' => 'aside.report.items'])
 </div>
 <div class="list-group list-group-accent">
     <div class="list-group-item list-group-item-accent-secondary bg-light text-center font-weight-bold text-muted text-uppercase small">Produtos</div>
