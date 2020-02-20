10aff93eae71a5a616ce840bbf141fac5ee3c532
Your Name <you@example.com>
2020-02-20 18:39:30 -0300

    .

--- Entities/OrderShippingCompany.php
+++ Entities/OrderShippingCompany.php
@@ -3,8 +3,15 @@
 namespace Modules\Order\Entities;
 
 use Illuminate\Database\Eloquent\Model;
+use Modules\Client\Entities\ShippingCompany;
 
 class OrderShippingCompany extends Model
 {
     protected $fillable = ['order_id', 'shipping_company_id'];
+
+    public function shipping_company()
+	{
+		return $this->belongsTo(ShippingCompany::class);
+	}
+
 }
